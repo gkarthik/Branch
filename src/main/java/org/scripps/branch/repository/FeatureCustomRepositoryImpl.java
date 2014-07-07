3a6430336d01293f0de3a4e4011c4e0e9283d740
Karthik <xraydiffraction@gmail.com>
2014-07-07 16:29:06 -0700

    customized Featurerepo for console output

--- src/main/java/org/scripps/branch/repository/FeatureCustomRepositoryImpl.java
+++ src/main/java/org/scripps/branch/repository/FeatureCustomRepositoryImpl.java
@@ -46,7 +46,7 @@ public class FeatureCustomRepositoryImpl implements FeatureCustomRepository {
 			Query q = em.createQuery(query);
 
 			List<?> list = q.getResultList();
-			LOGGER.debug("Feature Counter =");
+		//	LOGGER.debug("Feature Counter =");
 			Iterator<?> it = list.iterator();
 			int featureCounter = 0, attributeCounter = 0;
 
@@ -67,7 +67,7 @@ public class FeatureCustomRepositoryImpl implements FeatureCustomRepository {
 
 				}
 
-				LOGGER.debug("Feature Counter : " + featureCounter);
+			//	LOGGER.debug("Feature Counter : " + featureCounter);
 
 				List<Attribute> atts = getDataset_attributes();
 				if (atts == null) {
