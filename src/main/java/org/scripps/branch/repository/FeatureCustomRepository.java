eb67db92f04cf547e2136fa980b5b5c746a54b64
vysakh <vysakh@localhost>
2014-07-07 14:30:51 -0700

    made changes to FeatureRepo for getMetabricDataSet

--- src/main/java/org/scripps/branch/repository/FeatureCustomRepository.java
+++ src/main/java/org/scripps/branch/repository/FeatureCustomRepository.java
@@ -4,14 +4,11 @@ import java.util.Map;
 
 import org.scripps.branch.entity.Feature;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 public interface FeatureCustomRepository {
 
 	public Map<String, Feature> getByDataset(String dataset,
 			boolean load_annotations_very_slowly);
 
-	public ObjectNode getMetaBricClinicalFeatures(ObjectMapper mapper);
+	// public ObjectNode getMetaBricClinicalFeatures(ObjectMapper mapper);
 
 }
--- src/main/java/org/scripps/branch/repository/FeatureRepository.java
+++ src/main/java/org/scripps/branch/repository/FeatureRepository.java
@@ -15,11 +15,13 @@ public interface FeatureRepository extends JpaRepository<Feature, Long> {
 	@Query("select F from Feature F where F.id=?1")
 	Feature getByDbId(long id);
 
+	// not tested -- s
+	@Query("select F,A from Feature F, Attribute A where A.dataset='metabric_with_clinical' and F.id = feature_id and F.unique_id like 'metabric%'")
+	Feature getMetaBricClinicalFeatures();
+
 	// updatebyuniqueid need to check if parameters to be passes
 	@Query("update Feature set short_name=?1, long_name=?2, description=?3 where unique_id=?4")
 	public int updateByUniqueId(String short_name, String long_name,
 			String description, String unique_id);
-
-	// insert?
 
 }
