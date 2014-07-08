644137ce55c19eef94fb7e84288bfaf79dc79013
vyshakh <vyshakh@localhost>
2014-07-08 10:13:35 -0700

    completed custom_repository : except-cuxtom_feature_feature --- to be done

--- src/main/java/org/scripps/branch/repository/Custom_Feature_Repository.java
+++ src/main/java/org/scripps/branch/repository/Custom_Feature_Repository.java
@@ -11,12 +11,17 @@ import org.springframework.data.jpa.repository.Query;
 @Transactional
 public interface Custom_Feature_Repository {
 
+	@Query("insert into custom_feature(name,expression, description, user, dataset) values(1?,2?,3?,4?,5?)")
 	public int insert(String name, String feature_exp, String description,
-			int userid, List<Feature> features, String dataset);
+			int user, String dataset);
 
 	@SuppressWarnings("rawtypes")
 	@Query("select cf from custom_feature cf "
-			+ "where (cf.name like ('%' || lower(:searchText) || '%')) "
-			+ " or (cf.description like ('%' || lower(:searchText) || '%'))")
+			+ "where (cf.name like ('%' || (:searchText) || '%')) "
+			+ " or (cf.description like ('%' || (:searchText) || '%'))")
 	ArrayList searchCustomFeatures(String searchText);
+	
+
+	//custom_Feature_Feature
+
 }
