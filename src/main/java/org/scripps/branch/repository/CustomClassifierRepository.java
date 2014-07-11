09a34187f76444680b6a6ea7edaf6a17dd61b08c
karthik <karthik@localhost>
2014-07-11 15:35:30 -0700

    Aggregate Nodes working.

--- src/main/java/org/scripps/branch/repository/CustomClassifierRepository.java
+++ src/main/java/org/scripps/branch/repository/CustomClassifierRepository.java
@@ -7,15 +7,17 @@ import org.scripps.branch.entity.CustomClassifier;
 import org.scripps.branch.entity.Feature;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomClassifierRepository extends
 		JpaRepository<CustomClassifier, Long> {
 
 	@Query("select C.feature from CustomClassifier C where C.id=?1")
 	List<Feature> getClassifierByCustomClassifierId(long id);
 
-	@Query("select CC from CustomClassifier CC where name LIKE concat('%',concat(:name,'%')) or description LIKE concat('%',concat(:name,'%'))")
-	ArrayList<CustomClassifier> searchCustomClassifiers(String name);
+	@Query("select C from CustomClassifier C where C.name like concat('%',concat(?1,'%')) or C.Description like concat('%',concat(?1,'%'))")
+	List<CustomClassifier> searchCustomClassifiers(String name);
 	
 	CustomClassifier findById(long id);
 }
