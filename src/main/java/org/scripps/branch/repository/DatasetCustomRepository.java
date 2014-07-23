4cb58f268b8bfafcd87f4ca9d8a8b962e7e133d7
vyshakh <vyshakh@localhost>
2014-07-23 11:23:42 -0700

    adding and removing the files for upload module

---
+++ src/main/java/org/scripps/branch/repository/DatasetCustomRepository.java
@@ -0,0 +1,8 @@
+package org.scripps.branch.repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public interface DatasetCustomRepository {
+
+}
---
+++ src/main/java/org/scripps/branch/repository/DatasetCustomRepositoryImpl.java
@@ -0,0 +1,11 @@
+package org.scripps.branch.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DatasetCustomRepositoryImpl implements
+		DatasetCustomRepository {
+
+	
+	
+}
