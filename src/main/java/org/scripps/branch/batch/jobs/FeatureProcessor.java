9c4035827e2e17ed65b615ab479fec3272f145c6
Karthik <xraydiffraction@gmail.com>
2014-07-30 17:09:38 -0700

    Added logger.

--- src/main/java/org/scripps/branch/batch/jobs/FeatureProcessor.java
+++ src/main/java/org/scripps/branch/batch/jobs/FeatureProcessor.java
@@ -1,14 +1,18 @@
 package org.scripps.branch.batch.jobs;
 
+import org.scripps.branch.classifier.ManualTree;
 import org.scripps.branch.entity.Feature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
 public class FeatureProcessor implements ItemProcessor<Feature, Feature> {
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FeatureProcessor.class);
+	
 	@Override
 	public Feature process(Feature f) {
-		System.out.println("job");
-		System.out.println(f.getShort_name());
+		LOGGER.debug(f.getShort_name());	
 		return f;
 	}
 }
