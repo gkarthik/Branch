3e6b5e9117b181d558e40fc3f4dd385d6c9f360b
Karthik <xraydiffraction@gmail.com>
2014-08-04 15:36:15 -0700

    Using multiple datasets working for genes.

--- src/main/java/org/scripps/branch/batch/jobs/FeatureProcessor.java
+++ src/main/java/org/scripps/branch/batch/jobs/FeatureProcessor.java
@@ -2,17 +2,25 @@ package org.scripps.branch.batch.jobs;
 
 import org.scripps.branch.classifier.ManualTree;
 import org.scripps.branch.entity.Feature;
+import org.scripps.branch.repository.FeatureRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FeatureProcessor implements ItemProcessor<Feature, Feature> {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(FeatureProcessor.class);
 	
+	@Autowired 
+	FeatureRepository fRepo;
+	
 	@Override
 	public Feature process(Feature f) {
 		LOGGER.debug(f.getShort_name());	
-		return f;
+		if(fRepo.findByUniqueId(f.getUnique_id())==null){
+			return f;
+		}
+		return null;
 	}
 }
