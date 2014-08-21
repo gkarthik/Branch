b432e296ed80c19bb980730d36153b0d50e707fe
Karthik <xraydiffraction@gmail.com>
2014-08-21 13:51:37 -0700

    Change in AttributeServiceImpl.Delimitercheck

--- src/main/java/org/scripps/branch/service/CSV2Arff.java
+++ src/main/java/org/scripps/branch/service/CSV2Arff.java
@@ -2,27 +2,13 @@ package org.scripps.branch.service;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.supercsv.io.CsvBeanReader;
-import org.supercsv.io.CsvMapReader;
-import org.supercsv.io.ICsvMapReader;
-import org.supercsv.prefs.CsvPreference;
 
-import weka.classifiers.Classifier;
-import weka.classifiers.Evaluation;
-import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
@@ -73,91 +59,145 @@ public class CSV2Arff {
 	}
 
 
-		private static String delimiterCheck(String inputPath) {
+	//		private static String delimiterCheck(String inputPath) {
+	//
+	//		String line=null;
+	//		int noOfLinesToScan=100;
+	//		int count =0;
+	//		String[]tabs = null ;
+	//		String[] comma = null;
+	//		int a[] = new int[noOfLinesToScan] ;
+	//		int b[] = new int[noOfLinesToScan] ;
+	//		try {
+	//
+	//			BufferedReader fileReader = new BufferedReader(new FileReader(inputPath));
+	//
+	//			while ((line = fileReader.readLine()) != null && count<noOfLinesToScan)
+	//			{
+	//				tabs= line.split("\t");
+	//				comma=line.split(",");
+	//
+	//				a[count]=tabs.length;
+	//				b[count]=comma.length;	
+	//				count++;
+	//			}
+	//
+	//			//			
+	//			//			for (int i = 0; i < b.length; i++) {
+	//			//				System.out.println(b[i]);
+	//			//				System.out.println(a[i]);
+	//			//			}
+	//			//			System.out.println("tabs"+tabs.length);
+	//			//			System.out.println("commas"+comma.length);
+	//			//			int i=0,j=0;
+	//			//			if (a.length == 0 && b.length== 0) {
+	//			//				return "";
+	//			//			} 
+	//			//			else if(a.length>0){
+	//			//				int first = a[0];
+	//			//				for (int element : a) {
+	//			//					if (element != first) {
+	//			//						break;
+	//			//					}
+	//			//					i++;
+	//			//				}
+	//			//			}
+	//			//			else if(b.length>0){
+	//			//				int first = b[0];
+	//			//				for (int element : b) {
+	//			//					if (element != first) {
+	//			//						break;
+	//			//					}
+	//			//					j++;
+	//			//				}
+	//			//			}
+	//			//			System.out.println("i"+i);
+	//			//
+	//			//			System.out.println("j"+j);
+	//			//			if(i==noOfLinesToScan && i>j)
+	//			//				return "tab";
+	//			//			else if (j==noOfLinesToScan && j>i)
+	//			//				return "comma";
+	//			//			else
+	//			//				System.out.println("Nothin");
+	//
+	//		} catch (IOException e) {
+	//			// TODO Auto-generated catch block
+	//			e.printStackTrace();
+	//		}
+	//
+	//
+	//		if(tabs.length>comma.length){
+	//			return "tab";
+	//		}
+	//
+	//		else
+	//			return "comma";
+	//
+	//
+	//
+	//	}
+	//
+
+
+	private static String delimiterCheck(String inputPath) {
 
 		String line=null;
+		String prevLine=null;
 		int noOfLinesToScan=100;
 		int count =0;
 		String[]tabs = null ;
 		String[] comma = null;
-		int a[] = new int[noOfLinesToScan] ;
-		int b[] = new int[noOfLinesToScan] ;
-		try {
+		String[] c;
+		String[] d;
+		int tFlag=0, cFlag=0;
 
+		try {
 			BufferedReader fileReader = new BufferedReader(new FileReader(inputPath));
 
 			while ((line = fileReader.readLine()) != null && count<noOfLinesToScan)
 			{
-				tabs= line.split("\t");
-				comma=line.split(",");
+				if(prevLine!=null && line.length()>0 && line.charAt(0)!='#')
+				{
 
-				a[count]=tabs.length;
-				b[count]=comma.length;	
+					tabs=line.split("\t");
+					comma=line.split(",");
+
+					if(tabs.length<1 && comma.length<1 && (prevLine.split(",")).length < 1 && (prevLine.split("\t")).length <1) return "invalid";
+
+					if( prevLine.split(",").length == comma.length && comma.length>1 && comma.length> tabs.length){
+						cFlag=1;
+					}
+					else cFlag=0;
+
+					if(prevLine.split("\t").length==tabs.length && tabs.length>1 && tabs.length>comma.length)
+						tFlag=1;
+					else tFlag=0;
+				}
+				else{
+					prevLine =line;
+					fileReader.readLine();
+				}				
+				//prevLine=line;
 				count++;
 			}
 
-			//			
-			//			for (int i = 0; i < b.length; i++) {
-			//				System.out.println(b[i]);
-			//				System.out.println(a[i]);
-			//			}
-			//			System.out.println("tabs"+tabs.length);
-			//			System.out.println("commas"+comma.length);
-			//			int i=0,j=0;
-			//			if (a.length == 0 && b.length== 0) {
-			//				return "";
-			//			} 
-			//			else if(a.length>0){
-			//				int first = a[0];
-			//				for (int element : a) {
-			//					if (element != first) {
-			//						break;
-			//					}
-			//					i++;
-			//				}
-			//			}
-			//			else if(b.length>0){
-			//				int first = b[0];
-			//				for (int element : b) {
-			//					if (element != first) {
-			//						break;
-			//					}
-			//					j++;
-			//				}
-			//			}
-			//			System.out.println("i"+i);
-			//
-			//			System.out.println("j"+j);
-			//			if(i==noOfLinesToScan && i>j)
-			//				return "tab";
-			//			else if (j==noOfLinesToScan && j>i)
-			//				return "comma";
-			//			else
-			//				System.out.println("Nothin");
-
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			LOGGER.debug("Exception Occured while reading file"+e);
 			e.printStackTrace();
 		}
-
-
-		if(tabs.length>comma.length){
-			return "tab";
-		}
-
-		else
-			return "comma";
-
-
-
+		if(tFlag==1 && cFlag==0) return "tab";
+		else if(tFlag==0 && cFlag==1)return "comma";
+		else return "invalid";
 	}
+
+
 
 
 	public static void main(String[] args) throws Exception {
 		//String csvFile = "/home/bob/gene_mapping.csv";
 		String csvFile ="/home/bob/Dataset/1/oslo_mapping.txt";
-
-		//		String arffFileName = "/home/bob/r5x4-with-extra.arff";
+		//		String arffFileName = "/home/bob/r5x4-with-extra.arff";face
 		//		Instances data = load(csvFile);
 		//		toArff(data, arffFileName);
 		//		LOGGER.debug("Number of Attributes are : " + data.numAttributes());
@@ -172,6 +212,5 @@ public class CSV2Arff {
 		String a=delimiterCheck(csvFile);
 		System.out.println("Return Value: "+ a);
 		//System.out.println("Nothing");
-
 	}
 }
