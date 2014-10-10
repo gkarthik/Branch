0f2456479620dc3d5512c676b7e9bee33342cfc2
Karthik <xraydiffraction@gmail.com>
2014-10-10 16:47:35 -0700

    Added class to overide evaluateOnceAndPredict()

---
+++ src/main/java/org/scripps/branch/evaluation/Evaluation.java
@@ -0,0 +1,13 @@
+package org.scripps.branch.evaluation;
+
+import weka.core.Instances;
+
+public class Evaluation extends weka.classifiers.Evaluation{
+
+	public Evaluation(Instances data) throws Exception {
+		super(data);
+	}
+	
+	
+	
+}
